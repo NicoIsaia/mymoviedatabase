44bd86683795f4d3f482b087a232246ea8357231
Yongasoo <nicoisaia@outlook.com>
2024-03-25 18:29:22 -0300

    Created the DatabaseDAO abstract class to extend classes pertinent to each table

--- src/main/java/com/mycompany/mymoviedatabase/DAO/DatabaseDAO.java
+++ src/main/java/com/mycompany/mymoviedatabase/DAO/DatabaseDAO.java
@@ -1,10 +1,31 @@
 
 package com.mycompany.mymoviedatabase.DAO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 /**
  *
  * @author NicoIsaia
  */
-public class DatabaseDAO {
-    // Here I will create the Data Access Object
+public abstract class DatabaseDAO {
+    protected Connection conn;
+
+    public DatabaseDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+    
+    public void closeConnection() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
 }
